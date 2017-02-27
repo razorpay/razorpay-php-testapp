66357def6a31d10eb48eba1ed64bd0a971f358d2
mayankamencherla <mayank.amencherla@razorpay.com>
2017-02-27 21:50:54 +0530

    [orders api] Added orders api to automatic checkout

--- automatic/charge.php
+++ automatic/charge.php
@@ -3,33 +3,67 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//Load API, Ideally it should installed by composer and autoloaded if your project uses composer
-require('../razorpay-php/Razorpay.php');
+session_start();
 
-use Razorpay\Api\Api;
+$keySecret = '<Enter your key secret>';
 
-$keyId = '<Your API KEY>';
-$keySecret = '<Your Key Secret>';
+$success = false;
+$error = "";
 
-//Use your key_id and key secret
-$api = new Api($keyId, $keySecret);
-
-//This is submited by the checkout form
-if (isset($_POST['razorpay_payment_id']) === false)
+if (empty($_POST['razorpay_payment_id']) === false)
 {
-    die("Payment id not provided");
+    $razorpayOrderId = $_SESSION['razorpay_order_id'];
+    $razorpayPaymentId = $_POST['razorpay_payment_id'];
+    $razorpaySignature = $_POST['razorpay_signature'];
+
+    $signature = hash_hmac('sha256', $razorpayOrderId . '|' . $razorpayPaymentId, $keySecret);
+
+    // This method is defined only for php 5.6.0 and later
+    if (string_equals($signature , $razorpaySignature))
+    {
+        $success = true;
+    }
+    else 
+    {
+        $error = "Payment Failed";
+    }
 }
 
-$id = $_POST['razorpay_payment_id'];
+if ($success === true)
+{
+    $html = "<p>Your payment was successful</p>
+             <p>Payment ID: {$razorpayPaymentId}</p>";
+}
+else 
+{
+    $html = "<p>Your payment failed</p>
+             <p>Error Message: {$error}</p>";
+}
 
-//capture Rs 5100
-$payment = $api->payment->fetch($id)->capture(array('amount'=>5100));
+echo $html;
 
-//echo response
-echo json_encode($payment->toArray());
-
-//Payment is captured, do whatever else you need to do
-// Mark order as done using the submitted hidden field
-$shopping_order_id = $_POST['shopping_order_id'];
-// markOrderAsDone($shopping_order_id);
-?>
+/*
+ * Taken from https://stackoverflow.com/questions/10576827/secure-string-compare-function
+ * under the MIT license
+ * For versions later than php 5.6.0, hash_equals is used
+ * For newer versions, this manual method of comparing strings is used
+ */
+function string_equals($str1, $str2)
+{
+    if (function_exists('hash_equals'))
+    {
+        return hash_equals($str1, $str2);
+    }
+    if (strlen($str1) !== strlen($str2)) 
+    {
+        return false;
+    }
+    $result = 0;
+    
+    for ($i = 0; $i < strlen($str1); $i++) 
+    {
+        $result |= ord($str1[$i]) ^ ord($str2[$i]);
+    }
+    
+    return ($result == 0);
+}
---
+++ automatic/order.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+
+require('../razorpay-php/Razorpay.php');
+use Razorpay\Api\Api;
+
+$keyId = '<Enter your key id>';
+$keySecret = '<Enter your key secret>';
+
+$api = new Api($keyId, $keySecret);
+
+$orderData = [
+    'receipt'         => 3456,
+    'amount'          => 2000 * 100, // 2000 rupees in paise
+    'currency'        => 'INR',
+    'payment_capture' => 1 // auto capture 
+];
+
+$razorpayOrder = $api->order->create($orderData);
+
+$razorpayOrderId = $razorpayOrder['id'];
+
+$_SESSION['razorpay_order_id'] = $razorpayOrderId;
+
+$amount = $orderData['amount'];
+
+$html = <<<EOT
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+</head>
+<body>
+    <form action="charge.php" method="POST">
+      <script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        data-key="$keyId"
+        data-amount="$amount"
+        data-name="Daft Punk"
+        data-description="Purchase Description"
+        data-image="vk.jpg"
+        data-netbanking="true"
+        data-description="Tron Legacy"
+        data-prefill.name="Harshil Mathur"
+        data-prefill.email="harshil@razorpay.com"
+        data-prefill.contact="9999999999"
+        data-notes.shopping_order_id="21"
+        data-order_id="$razorpayOrderId">
+      </script>
+      <!-- Any extra fields to be submitted with the form but not sent to Razorpay -->
+      <input type="hidden" name="shopping_order_id" value="3456">
+    </form>
+</body>
+</html>
+EOT;
+
+echo $html;
