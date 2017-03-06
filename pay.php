8478b910a6f004b5363123a2f6c65133bbc02660
Mayank Amencherla <mayankamencherla@users.noreply.github.com>
2017-03-06 17:03:05 +0530

    [orders api] Created the orders api test app in php (#5)
    * [orders api] Created the orders api test app in php
    
    [orders api] Moved php-sdk to the main folder
    
    * [orders api] Made minor improvements to the code and readme
    
    * [orders api] Added a demo.html file
    
    * [orders api] Added orders api to automatic checkout
    
    * [orders api] Added a config.php.sample to the code base
    
    * [orders api] Added a config.php.sample and instructions on how to use the app
    
    * [orders api] Made minor improvments
    
    [orders api] Removed DS_Store files
    
    * added the latest php sdk
    
    * [multi currency] Added multi currency support for php test app
    
    * Restructures the code slightly
    
    * Adds optional checkout fields with default values as doc
    
    * Updates README to reflect new demo location
    
    * Made minor improvements
    
    * [orders api] Made several minor bug fixes
    
    * [orders api] Made minor improvements
    
    * [orders api] Fixed bug in form
    
    * [php sdk] Added latest sdk to test app
    
    * [php sdk] Fixed error in php sdk
    
    * Made minor improvements
    
    * Made minor improvements

---
+++ pay.php
@@ -0,0 +1,76 @@
+<?php
+
+require('config.php');
+require('razorpay-php/Razorpay.php');
+session_start();
+
+// Create the Razorpay Order
+
+use Razorpay\Api\Api;
+
+$api = new Api($keyId, $keySecret);
+
+//
+// We create an razorpay order using orders api
+// Docs: https://docs.razorpay.com/docs/orders
+//
+$orderData = [
+    'receipt'         => 3456,
+    'amount'          => 2000 * 100, // 2000 rupees in paise
+    'currency'        => 'INR',
+    'payment_capture' => 1 // auto capture
+];
+
+$razorpayOrder = $api->order->create($orderData);
+
+$razorpayOrderId = $razorpayOrder['id'];
+
+$_SESSION['razorpay_order_id'] = $razorpayOrderId;
+
+$displayAmount = $amount = $orderData['amount'];
+
+if ($displayCurrency !== 'INR')
+{
+    $url = "https://api.fixer.io/latest?symbols=$displayCurrency&base=INR";
+    $exchange = json_decode(file_get_contents($url), true);
+
+    $displayAmount = $exchange['rates'][$displayCurrency] * $amount / 100;
+}
+
+$checkout = 'automatic';
+
+if (isset($_GET['checkout']) and in_array($_GET['checkout'], ['automatic', 'manual'], true))
+{
+    $checkout = $_GET['checkout'];
+}
+
+$data = [
+    "key"               => $keyId,
+    "amount"            => $amount,
+    "name"              => "DJ Tiesto",
+    "description"       => "Tron Legacy",
+    "image"             => "https://s29.postimg.org/r6dj1g85z/daft_punk.jpg",
+    "prefill"           => [
+    "name"              => "Daft Punk",
+    "email"             => "customer@merchant.com",
+    "contact"           => "9999999999",
+    ],
+    "notes"             => [
+    "address"           => "Hello World",
+    "merchant_order_id" => "12312321",
+    ],
+    "theme"             => [
+    "color"             => "#F37254"
+    ],
+    "order_id"          => $razorpayOrderId,
+];
+
+if ($displayCurrency !== 'INR')
+{
+    $data['display_currency']  = $displayCurrency;
+    $data['display_amount']    = $displayAmount;
+}
+
+$json = json_encode($data);
+
+require("checkout/{$checkout}.php");
